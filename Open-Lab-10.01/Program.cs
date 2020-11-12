1ea697f47cf883bb376fb4851c1906eb7d5638cb
PC <PC@ANDREJKOVPOITA>
2020-11-12 18:35:17 +0100

    Add project files.

---
+++ Open-Lab-10.01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Open_Lab_10._00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book LOTR = new Book();
+            LOTR.SetBook("Plebovia a ich vyskyt", 888);
+            Console.ReadKey();
+        }
+    }
+    class Book
+    {
+        public void SetBook(string title, int pages)
+        {
+            Console.Write("Book " + title + " has " + pages);
+            Console.WriteLine(" pages.");
+        }
+    }
+
+}
